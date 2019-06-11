29280866dd00dd661a27f50d60e6113c4acc2c9d
qrz136185 <100652601@qq.com>
2019-06-11 18:13:06 +0800

    dfgghghh

--- src/main/java/com/example/springtest/SpringTestApplication.java
+++ src/main/java/com/example/springtest/SpringTestApplication.java
@@ -9,6 +9,7 @@ public class SpringTestApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringTestApplication.class, args);
         System.out.println("启动成功");
+        System.out.println("启动成功1");
     }
 
 }
